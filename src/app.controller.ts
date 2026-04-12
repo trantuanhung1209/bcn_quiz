570afeafbbb8f68f9a1080fabe2e5f97bb7fceda
trantuanhung <23659581.hung@student.iuh.edu.vn>
2026-04-12 19:37:35 +0700

    update(health): add public for checking health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Roles } from './auth/decorators/roles.decorator';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
